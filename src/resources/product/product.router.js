1ca759c98e8e631fd67520c6809936733637e0cf
Shola Ayeni <ayenisholah@yahoo.com>
2020-08-10 15:26:34 +0100

    created product router

---
+++ src/resources/product/product.router.js
@@ -0,0 +1,16 @@
+import { Router } from 'express'
+import controllers from './product.controllers'
+
+const router = Router()
+
+// /api/product
+router.route('/').get(controllers.getOne).post(controllers.createOne)
+
+// /api/product/:id
+router
+  .route('/:id')
+  .get(controllers.getOne)
+  .put(controllers.updateOne)
+  .delete(controllers.removeOne)
+
+export default router
