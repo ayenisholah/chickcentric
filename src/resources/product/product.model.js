dde88e237dcbd69cbe165e2f16a86ab3f27b190f
Shola Ayeni <ayenisholah@yahoo.com>
2020-08-10 15:26:45 +0100

    created product model

---
+++ src/resources/product/product.model.js
@@ -0,0 +1,42 @@
+import mongoose from 'mongoose'
+
+const productSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+      trim: true,
+      maxlength: 255,
+    },
+    imageUrl: {
+      type: String,
+      required: true,
+    },
+    inCart: {
+      type: Boolean,
+      required: false,
+    },
+    count: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    total: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+)
+
+export const Product = mongoose.model('product', productSchema)
