28da89b917a70c8fb111be76850de807500e96be
Shola Ayeni <ayenisholah@yahoo.com>
2020-08-10 15:28:04 +0100

    server entry point

---
+++ src/server.js
@@ -0,0 +1,43 @@
+import express from 'express'
+import { json, urlencoded } from 'body-parser'
+import morgan from 'morgan'
+import cors from 'cors'
+import config from './config'
+import { signup, signin, protect } from './utils/auth'
+import { connect } from './utils/db'
+import productRouter from './resources/product/product.router'
+
+export const app = express()
+
+app.disable('x-powered-by')
+
+app.use(cors())
+app.use(json())
+app.use(urlencoded({ extended: true }))
+app.use(morgan('dev'))
+
+app.get('/', (_, res) =>
+  res
+    .status(200)
+    .json({ status: 200, message: 'Welcome, the chick centric app awaits!' })
+)
+
+app.post('/signup', signup)
+app.post('/signin', signin)
+
+app.use('/api', protect)
+app.use('/api/product', productRouter)
+
+export const start = async () => {
+  try {
+    await connect()
+    app.listen(config.port, () => {
+      console.log(
+        '\x1b[35m%s\x1b[0m',
+        `Magic happening on http://localhost:${config.port}`
+      )
+    })
+  } catch (e) {
+    console.error(e)
+  }
+}
