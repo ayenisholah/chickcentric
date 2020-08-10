07bd2ae71225c729ce4c1956b45938365e004a53
Shola Ayeni <ayenisholah@yahoo.com>
2020-08-10 17:25:02 +0100

    switched to local cosmosdb

--- src/config/dev.js
+++ src/config/dev.js
@@ -2,6 +2,5 @@ export const config = {
   secrets: {
     jwt: 'learneverything',
   },
-  dbUrl:
-    'mongodb://chickcentric:hiR8b8IvrdUjtNhf1JEcaBDeT7SpNUCtQOYRrZgiDBlzsw1H0rbqZxHmWh9jZXf0F3zSJIJg60rw9HYLCxRrUQ==@chickcentric.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@chickcentric@',
+  dbUrl: 'mongodb://localhost:27017/chickcentric-dev',
 }
