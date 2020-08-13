cbf3a2835713ce582875bacc5fa386582ebf91bf
Shola Ayeni <ayenisholah@yahoo.com>
2020-08-13 17:05:32 +0100

    created landing hero

---
+++ src/components/home/Landing.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Landing() {
+  return (
+    <div className="landing">
+      <section className="hero">
+        <h1>Welcome to Chick Centric</h1>
+        <p>
+          Our designs are inspired by the people and world around us. Beautiful,
+          high quality dresses that are designed especially for you. Discover
+          our story and meet the people that make our brand what it is.
+        </p>
+      </section>
+    </div>
+  );
+}
+
+export default Landing;
