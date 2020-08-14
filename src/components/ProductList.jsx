9240aef96a753d888e81285b0f20456002c2e001
Shola Ayeni <ayenisholah@yahoo.com>
2020-08-14 11:48:46 +0100

    created product list component

---
+++ src/components/ProductList.jsx
@@ -0,0 +1,63 @@
+import React, { Component } from "react";
+import axios from "axios";
+import Product from "./Product";
+import Title from "./Title";
+import styled from "styled-components";
+import { ProductConsumer } from "../context";
+export default class ProductList extends Component {
+  state = {
+    products: [],
+  };
+  componentDidMount() {
+    this.setProducts();
+  }
+
+  setProducts = async () => {
+    let products = [];
+
+    let { data } = await axios.get(
+      "https://lit-sands-58479.herokuapp.com/api/product",
+      {
+        headers: {
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVmMzMxNmRkOWFjMWZjMDAxN2IzYTgzNCIsImlhdCI6MTU5NzIzNTg3NCwiZXhwIjoxNjA1ODc1ODc0fQ.EcgTyioeNG7fOh2Q8DsNz3DhVw5RDFCSQeIVqQwO1ok",
+        },
+      }
+    );
+
+    data.data.forEach((item) => {
+      const singleItem = { ...item };
+      products = [...products, singleItem];
+    });
+
+    this.setState(() => {
+      return { products };
+    }, this.checkCartItems);
+
+    console.log("products", products);
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <ProductWrapper className="py-5">
+          <div className="container" style={{ fontWeight: "1.2rem" }}>
+            <Title name="our" title="collections" />
+            <div className="row">
+              <ProductConsumer>
+                {(value) => {
+                  console.log(value.products);
+                  return value.products.map((product) => {
+                    return <Product key={product._id} product={product} />;
+                  });
+                }}
+              </ProductConsumer>
+            </div>
+          </div>
+        </ProductWrapper>
+      </React.Fragment>
+    );
+  }
+}
+
+const ProductWrapper = styled.section``;
