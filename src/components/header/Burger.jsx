ccf16c37fef375e8f074e630233f384236624b70
Shola Ayeni <ayenisholah@yahoo.com>
2020-08-14 13:54:53 +0100

    edittd harmburger color

--- src/components/header/Burger.jsx
+++ src/components/header/Burger.jsx
@@ -16,7 +16,7 @@ const StyledBurger = styled.div`
     div {
       width: 2rem;
       height: 0.25rem;
-      background-color: ${({ open }) => (open ? "#ffffff" : "#ffffff")};
+      background-color: ${({ open }) => (open ? "#404040" : "#404040")};
       border-radius: 10px;
       transform-origin: 1px;
       transition: all 0.3s linear;
