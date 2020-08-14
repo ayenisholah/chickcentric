53fa766c55b6f72b5cf58dafe7f8d3a3a4b35d0d
Shola Ayeni <ayenisholah@yahoo.com>
2020-08-14 11:41:11 +0100

    imported components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
 import Navbar from "./components/header/Navbar";
 import Landing from "./components/home/Landing";
+import Footer from "./components/home/Footer";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.scss";
+import Cart from "./components/Cart/Cart";
+import Details from "./components/Details";
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <Landing />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Landing} />
+          <Route path="/details" component={Details} />
+          <Route path="/cart" component={Cart} />
+        </Switch>
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
